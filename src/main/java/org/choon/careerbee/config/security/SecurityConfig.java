86d47d2b19555574b6d261643d2ec1d69fbb5be0
박성춘 <songchoon10@naver.com>
2025-05-02 14:52:09 +0900

    chore: cors 설정

--- src/main/java/org/choon/careerbee/config/security/SecurityConfig.java
+++ src/main/java/org/choon/careerbee/config/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
           "http://localhost:5500",
           "http://127.0.0.1:5173",
           "http://127.0.0.1:5500",
-          "https://careerbee.co.kr",
+          "https://www.careerbee.co.kr",
           "https://ai.careerbee.co.kr"
       ));
       config.setAllowCredentials(true);
