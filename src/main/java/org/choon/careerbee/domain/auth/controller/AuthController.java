50dfbbad37ac570eae1ab466ad5c342afe0ec523
박성춘 <songchoon10@naver.com>
2025-05-13 10:28:30 +0900

    refactor: 공통응답 구조 변경
    Detail
    싱글톤인 Enum의 message 필드가 예기치 않게 중복 사용되는 문제를 해결하기 위해 응답 구조를 변경하였습니다.
    - 이는 Enum을 Non-Immutable하게 사용해 발생하는 문제였습니다.

--- src/main/java/org/choon/careerbee/domain/auth/controller/AuthController.java
+++ src/main/java/org/choon/careerbee/domain/auth/controller/AuthController.java
@@ -8,8 +8,8 @@ import org.choon.careerbee.common.dto.ApiResponse;
 import org.choon.careerbee.common.dto.ApiResponseEntity;
 import org.choon.careerbee.common.enums.CustomResponseStatus;
 import org.choon.careerbee.domain.auth.dto.jwt.AuthTokens;
-import org.choon.careerbee.domain.auth.dto.response.OAuthLoginUrlResp;
 import org.choon.careerbee.domain.auth.dto.response.LoginResp;
+import org.choon.careerbee.domain.auth.dto.response.OAuthLoginUrlResp;
 import org.choon.careerbee.domain.auth.dto.response.ReissueResp;
 import org.choon.careerbee.domain.auth.dto.response.TokenAndUserInfo;
 import org.choon.careerbee.domain.auth.service.auth.AuthService;
@@ -29,69 +29,74 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/api/v1/auth")
 public class AuthController {
-  private final AuthService authService;
 
-  @GetMapping("/oauth")
-  public ResponseEntity<ApiResponse<OAuthLoginUrlResp>> getOAuthLoginUrl(
-      @RequestParam(value = "type") String type
-  ) {
-    OAuthLoginUrlResp response = authService.getOAuthLoginUrl(type);
+    private final AuthService authService;
 
-    return ApiResponseEntity.ok(
-        response,
-        CustomResponseStatus.SUCCESS.withMessage("소셜 로그인 url 조회에 성공하였습니다.")
-    );
-  }
+    @GetMapping("/oauth")
+    public ResponseEntity<ApiResponse<OAuthLoginUrlResp>> getOAuthLoginUrl(
+        @RequestParam(value = "type") String type
+    ) {
+        OAuthLoginUrlResp response = authService.getOAuthLoginUrl(type);
 
-  @PostMapping("/oauth/tokens/kakao")
-  public ResponseEntity<ApiResponse<LoginResp>> kakaoLogin(
-      @RequestBody KakaoLoginParams kakaoParams,
-      HttpServletResponse response
-  ) {
-    TokenAndUserInfo tokenAndUserInfo = authService.login(kakaoParams);
-    setTokenInCookie(response, tokenAndUserInfo.authTokens());
+        return ApiResponseEntity.ok(
+            response,
+            CustomResponseStatus.SUCCESS,
+            "소셜 로그인 url 조회에 성공하였습니다."
+        );
+    }
 
-    return ApiResponseEntity.ok(
-        new LoginResp(tokenAndUserInfo.authTokens().accessToken(), tokenAndUserInfo.userInfo()),
-        CustomResponseStatus.SUCCESS.withMessage("로그인에 성공하였습니다.")
-    );
-  }
+    @PostMapping("/oauth/tokens/kakao")
+    public ResponseEntity<ApiResponse<LoginResp>> kakaoLogin(
+        @RequestBody KakaoLoginParams kakaoParams,
+        HttpServletResponse response
+    ) {
+        TokenAndUserInfo tokenAndUserInfo = authService.login(kakaoParams);
+        setTokenInCookie(response, tokenAndUserInfo.authTokens());
 
-  @PostMapping("/logout")
-  public ResponseEntity<ApiResponse<Void>> logout(
-      @RequestHeader("Authorization") String accessToken
-  ) {
-    authService.logout(accessToken);
+        return ApiResponseEntity.ok(
+            new LoginResp(tokenAndUserInfo.authTokens().accessToken(), tokenAndUserInfo.userInfo()),
+            CustomResponseStatus.SUCCESS,
+            "로그인에 성공하였습니다."
+        );
+    }
 
-    return ApiResponseEntity.ok(
-        CustomResponseStatus.SUCCESS_WITH_NO_CONTENT.withMessage("로그아웃에 성공하였습니다.")
-    );
-  }
+    @PostMapping("/logout")
+    public ResponseEntity<ApiResponse<Void>> logout(
+        @RequestHeader("Authorization") String accessToken
+    ) {
+        authService.logout(accessToken);
 
-  @PostMapping("/reissue")
-  public ResponseEntity<ApiResponse<ReissueResp>> reissue(
-      @CookieValue("refreshToken") String refreshToken,
-      HttpServletResponse response
-  ) {
-    AuthTokens authTokens = authService.reissue(refreshToken);
-    setTokenInCookie(response, authTokens);
+        return ApiResponseEntity.ok(
+            CustomResponseStatus.SUCCESS_WITH_NO_CONTENT,
+            "로그아웃에 성공하였습니다."
+        );
+    }
 
-    return ApiResponseEntity.ok(
-        new ReissueResp(authTokens.accessToken()),
-        CustomResponseStatus.SUCCESS.withMessage("토큰 재발급에 성공하였습니다.")
-    );
-  }
+    @PostMapping("/reissue")
+    public ResponseEntity<ApiResponse<ReissueResp>> reissue(
+        @CookieValue("refreshToken") String refreshToken,
+        HttpServletResponse response
+    ) {
+        AuthTokens authTokens = authService.reissue(refreshToken);
+        setTokenInCookie(response, authTokens);
 
-  private void setTokenInCookie(HttpServletResponse response, AuthTokens tokens) {
-    response.addCookie(createCookie(tokens.refreshToken()));
-  }
+        return ApiResponseEntity.ok(
+            new ReissueResp(authTokens.accessToken()),
+            CustomResponseStatus.SUCCESS,
+            "토큰 재발급에 성공하였습니다."
+        );
+    }
 
-  private Cookie createCookie(String value) {
-    Cookie cookie = new Cookie("refreshToken", value);
-    cookie.setHttpOnly(true);
-    cookie.setSecure(true);
-    cookie.setPath("/");
+    private void setTokenInCookie(HttpServletResponse response, AuthTokens tokens) {
+        response.addCookie(createCookie(tokens.refreshToken()));
+    }
+
+    private Cookie createCookie(String value) {
+        Cookie cookie = new Cookie("refreshToken", value);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
 //    cookie.setDomain(cookieDomain);
-    return cookie;
-  }
+        return cookie;
+    }
 }
