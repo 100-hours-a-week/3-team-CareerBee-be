d4233ee45c527f5743237f3eabd8dec987f52c55
박성춘 <songchoon10@naver.com>
2025-05-14 21:43:18 +0900

    feat: Company API에 대한 Swagger 작성 완료

--- src/main/java/org/choon/careerbee/domain/company/controller/CompanyQueryController.java
+++ src/main/java/org/choon/careerbee/domain/company/controller/CompanyQueryController.java
@@ -1,5 +1,7 @@
 package org.choon.careerbee.domain.company.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.choon.careerbee.common.dto.ApiResponseEntity;
@@ -29,9 +31,17 @@ public class CompanyQueryController {
 
     private final CompanyQueryService queryService;
 
+    @Operation(
+        summary = "기업 거리 기반 조회",
+        description = "해당 주소 정보와 조건에 맞는 기업을 거리순으로 조회합니다.",
+        tags = {"Company"}
+    )
     @GetMapping
     public ResponseEntity<CommonResponse<CompanyRangeSearchResp>> fetchCompaniesByDistance(
+        @Parameter(description = "조회 기준이 되는 주소 정보 (위도, 경도, 반경 등)")
         @ModelAttribute CompanyQueryAddressInfo companyQueryAddressInfo,
+
+        @Parameter(description = "기업 필터링 조건 (조회 반경)")
         @ModelAttribute CompanyQueryCond companyQueryCond
     ) {
         CompanyRangeSearchResp response = queryService.fetchCompaniesByDistance(
@@ -44,6 +54,10 @@ public class CompanyQueryController {
         );
     }
 
+    @Operation(
+        summary = "기업 간단 정보 조회",
+        description = "기업 ID에 따른 요약 정보를 조회합니다."
+    )
     @GetMapping("/{companyId}/summary")
     public ResponseEntity<CommonResponse<CompanySummaryInfo>> fetchCompaniesSummary(
         @PathVariable Long companyId
@@ -57,8 +71,14 @@ public class CompanyQueryController {
         );
     }
 
+    @Operation(
+        summary = "기업 상세 정보 조회",
+        description = "기업 ID에 따른 상세 정보를 조회합니다.",
+        tags = {"Company"}
+    )
     @GetMapping("/{companyId}")
     public ResponseEntity<CommonResponse<CompanyDetailResp>> fetchCompanyDetail(
+        @Parameter(description = "기업 ID", example = "1")
         @PathVariable Long companyId
     ) {
         CompanyDetailResp response = queryService.fetchCompanyDetail(companyId);
@@ -70,8 +90,14 @@ public class CompanyQueryController {
         );
     }
 
+    @Operation(
+        summary = "기업 검색",
+        description = "키워드를 바탕으로 기업을 검색합니다.",
+        tags = {"Company"}
+    )
     @GetMapping("/search")
     public ResponseEntity<CommonResponse<CompanySearchResp>> fetchCompanyDetail(
+        @Parameter(description = "검색 키워드", example = "네이버")
         @RequestParam(value = "keyword") String keyword
     ) {
         CompanySearchResp response = queryService.fetchMatchingCompaniesByKeyword(keyword);
@@ -83,8 +109,14 @@ public class CompanyQueryController {
         );
     }
 
+    @Operation(
+        summary = "특정 기업 위치 정보 조회",
+        description = "특정 기업의 위치 정보를 조회합니다.",
+        tags = {"Company"}
+    )
     @GetMapping("/{companyId}/locations")
     public ResponseEntity<CommonResponse<CompanyMarkerInfo>> fetchCompanyLocationInfo(
+        @Parameter(description = "기업 ID", example = "1")
         @PathVariable Long companyId
     ) {
         CompanyMarkerInfo response = queryService.fetchCompanyLocation(companyId);
