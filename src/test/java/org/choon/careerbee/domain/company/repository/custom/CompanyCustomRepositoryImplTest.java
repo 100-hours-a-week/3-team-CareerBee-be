641c05df465a78d89016a5b4b2e2296e43f9406f
박성춘 <songchoon10@naver.com>
2025-05-26 20:39:39 +0900

    test(#103): 기업 검색 기능에 대한 Repository Layer 테스트코드 작성

--- src/test/java/org/choon/careerbee/domain/company/repository/custom/CompanyCustomRepositoryImplTest.java
+++ src/test/java/org/choon/careerbee/domain/company/repository/custom/CompanyCustomRepositoryImplTest.java
@@ -10,12 +10,15 @@ import org.choon.careerbee.domain.company.dto.request.CompanyQueryAddressInfo;
 import org.choon.careerbee.domain.company.dto.request.CompanyQueryCond;
 import org.choon.careerbee.domain.company.dto.response.CompanyDetailResp;
 import org.choon.careerbee.domain.company.dto.response.CompanyRangeSearchResp;
+import org.choon.careerbee.domain.company.dto.response.CompanySearchResp;
 import org.choon.careerbee.domain.company.entity.Company;
 import org.choon.careerbee.domain.company.entity.enums.BusinessType;
 import org.choon.careerbee.domain.company.entity.enums.CompanyType;
 import org.choon.careerbee.domain.company.entity.enums.RecruitingStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
@@ -44,11 +47,13 @@ class CompanyCustomRepositoryImplTest {
     @DisplayName("반경 내 기업 3개가 정상적으로 조회되는가")
     void fetchByDistanceAndCondition_반경내_여러기업조회() {
         // given
-        CompanyQueryAddressInfo addressInfo = new CompanyQueryAddressInfo(37.40024430415324, 127.10698761648364);
+        CompanyQueryAddressInfo addressInfo = new CompanyQueryAddressInfo(37.40024430415324,
+            127.10698761648364);
         CompanyQueryCond cond = new CompanyQueryCond(1000); // 500m
 
         // when
-        CompanyRangeSearchResp result = companyCustomRepository.fetchByDistanceAndCondition(addressInfo, cond);
+        CompanyRangeSearchResp result = companyCustomRepository.fetchByDistanceAndCondition(
+            addressInfo, cond);
 
         // then
         assertThat(result.companies()).hasSize(3);
@@ -59,14 +64,15 @@ class CompanyCustomRepositoryImplTest {
     void fetchCompanyDetailById_shouldReturnCompanyDetailResp() {
         // given
         Company company = createCompany(
-             "테스트 회사", 37.40203443, 127.1034665
+            "테스트 회사", 37.40203443, 127.1034665
         );
         em.persist(company);
         em.flush();
         em.clear();
 
         // when
-        CompanyDetailResp actualResp = companyCustomRepository.fetchCompanyDetailById(company.getId());
+        CompanyDetailResp actualResp = companyCustomRepository.fetchCompanyDetailById(
+            company.getId());
 
         // then
         assertThat(actualResp).isNotNull();
@@ -89,6 +95,54 @@ class CompanyCustomRepositoryImplTest {
         ).hasMessage(CustomResponseStatus.COMPANY_NOT_EXIST.getMessage());
     }
 
+    @ParameterizedTest
+    @CsvSource({
+        "카, 3",
+        "'  카', 0",
+        "테스트, 1",
+        "존재하지 않는 회사, 0",
+    })
+    @DisplayName("해당 키워드에 매칭되는 기업에 대해 정상적인 개수 반환")
+    void fetchMatchingCompaniesByKeyword_success(String keyword, Integer expectedCount) {
+        // given
+        Company kakao = createCompany("카카오", 37.123, 127.123);
+        Company kakaoHealth = createCompany("카카오 헬스케어", 37.123, 127.123);
+        Company testKakao = createCompany("테스트 카카오", 37.123, 127.123);
+
+        em.persist(kakao);
+        em.persist(kakaoHealth);
+        em.persist(testKakao);
+        em.flush();
+        em.clear();
+
+        // when
+        CompanySearchResp actualResp = companyCustomRepository.fetchMatchingCompaniesByKeyword(
+            keyword);
+
+        // then
+        assertThat(actualResp.matchingCompanies().size()).isEqualTo(expectedCount);
+    }
+
+    @Test
+    @DisplayName("기업 검색시 최대 8개까지만 조회되는지 확인")
+    void fetchMatchingCompaniesByKeyword_shouldReturnAtMost8Companies() {
+        // given
+        for (int i = 1; i <= 10; i++) {
+            Company company = createCompany("테스트 기업 " + i, 37.123 + i * 0.001, 127.123 + i * 0.001);
+            em.persist(company);
+        }
+        em.flush();
+        em.clear();
+
+        // when
+        CompanySearchResp actualResp = companyCustomRepository.fetchMatchingCompaniesByKeyword(
+            "테스트");
+
+        // then
+        assertThat(actualResp.matchingCompanies().size()).isEqualTo(8);
+    }
+
+
     private Company createCompany(
         String name,
         Double latitude,
@@ -101,7 +155,6 @@ class CompanyCustomRepositoryImplTest {
             .name(name)
             .geoPoint(geoPoint)
             .address("경기 성남시 분당구")
-            .homeUrl("https://homepage.com")
             .logoUrl("https://logo.com")
             .description("회사 소개")
             .companyType(CompanyType.ENTERPRISE)
