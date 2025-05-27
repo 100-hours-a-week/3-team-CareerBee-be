cf06350fab404b21146dc6f9db98828696507187
박성춘 <songchoon10@naver.com>
2025-05-27 16:51:25 +0900

    test(#103): escape 처리를 테스트하기 위한 Controller Layer 테스트코드 추가

--- src/test/java/org/choon/careerbee/domain/company/controller/CompanyControllerTest.java
+++ src/test/java/org/choon/careerbee/domain/company/controller/CompanyControllerTest.java
@@ -158,4 +158,33 @@ class CompanyControllerTest {
             .andExpect(jsonPath("$.data.matchingCompanies[0].name").value("카카오"))
             .andExpect(jsonPath("$.data.matchingCompanies[1].name").value("카카오 헬스케어"));
     }
+
+    @Test
+    @DisplayName("키워드로 기업 검색 시 escape 처리하여 결과 반환")
+    void searchCompanyByKeyword_shouldReturn200_withStripAndEscape() throws Exception {
+        // given
+        Company kakao = createCompany("카_오", 37.12, 127.13);
+        Company kakao_health = createCompany("카카오_헬스케어", 37.12, 127.13);
+        Company hyundai = createCompany("현대_자동차", 37.12, 127.13);
+        Company coupang = createCompany("쿠팡", 37.12, 127.13);
+
+        em.persist(kakao);
+        em.persist(kakao_health);
+        em.persist(hyundai);
+        em.persist(coupang);
+
+        em.flush();
+        em.clear();
+
+        // when & then
+        mockMvc.perform(get("/api/v1/companies/search")
+                .param("keyword", "_")
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.message").value("매칭 데이터 조회에 성공하였습니다."))
+            .andExpect(jsonPath("$.data.matchingCompanies.length()").value(3))
+            .andExpect(jsonPath("$.data.matchingCompanies[0].name").value("카_오"))
+            .andExpect(jsonPath("$.data.matchingCompanies[1].name").value("카카오_헬스케어"))
+            .andExpect(jsonPath("$.data.matchingCompanies[2].name").value("현대_자동차"));
+    }
 }
