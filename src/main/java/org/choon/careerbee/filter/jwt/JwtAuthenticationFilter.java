fc9807631277c293fb90895f3bd7d93c83eee15e
박성춘 <songchoon10@naver.com>
2025-05-28 22:12:05 +0900

    test(#123): 로그아웃 기능에 대한 Controller Layer 통합 테스트 작성중
    Detail
    - 현재 JwtAuthenticationFilter 에서 예외를 반환할 때 어떤 식으로 처리할지 고민중
    - forward 를 통해 Handler에서 처리하는 방식
    - objectMapper를 이용한 filter에서 바로 예외 response 처리

--- src/main/java/org/choon/careerbee/filter/jwt/JwtAuthenticationFilter.java
+++ src/main/java/org/choon/careerbee/filter/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package org.choon.careerbee.filter.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.choon.careerbee.common.dto.CommonResponse;
 import org.choon.careerbee.common.enums.CustomResponseStatus;
 import org.choon.careerbee.common.exception.CustomException;
 import org.choon.careerbee.domain.auth.entity.enums.TokenStatus;
@@ -28,14 +30,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
     private final TokenRepository tokenRepository;
+    private final ObjectMapper objectMapper;
 
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
         FilterChain filterChain) throws ServletException, IOException {
         String resolveToken = jwtUtil.resolveToken(request.getHeader(AUTHORIZATION));
 
         if (Objects.equals(resolveToken, "")) {
-            request.getRequestDispatcher("/exception/entrypoint/nullToken")
-                .forward(request, response);
+            writeErrorResponse(response, CustomResponseStatus.NULL_JWT);
+//            request.getRequestDispatcher("/exception/entrypoint/nullToken")
+//                .forward(request, response);
             return;
         }
 
@@ -61,6 +65,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             .isPresent()) {
             throw new CustomException(CustomResponseStatus.LOGOUT_MEMBER);
         }
+    }
+
+    private void writeErrorResponse(HttpServletResponse response, CustomResponseStatus status)
+        throws IOException {
+        response.setStatus(status.getHttpStatusCode());
+        response.setContentType("application/json;charset=UTF-8");
+
+        String responseBody = objectMapper.writeValueAsString(CommonResponse.createError(status));
+        response.getWriter().write(responseBody);
     }
 
     @Override
