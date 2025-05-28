aaaf4820f421a3c090b9c836266042aad7f873f4
박성춘 <songchoon10@naver.com>
2025-05-28 14:47:02 +0900

    chore: fixture 에서 만든 createCompany 메서드 사용하도록 변

--- src/test/java/org/choon/careerbee/domain/company/controller/CompanyControllerTest.java
+++ src/test/java/org/choon/careerbee/domain/company/controller/CompanyControllerTest.java
@@ -199,29 +199,4 @@ class CompanyControllerTest {
             .andExpect(jsonPath("$.httpStatusCode").value(
                 CustomResponseStatus.INVALID_INPUT_VALUE.getHttpStatusCode()));
     }
-
-    private Company createCompany(String name, double latitude, double longitude) {
-        GeometryFactory factory = new GeometryFactory(new PrecisionModel(), 4326);
-        Point geoPoint = factory.createPoint(new Coordinate(longitude, latitude));
-        return Company.builder()
-            .name(name)
-            .geoPoint(geoPoint)
-            .address("서울시 강남구")
-            .logoUrl("https://logo.test.com")
-            .description("테스트 기업 설명")
-            .recentIssue("테스트 이슈")
-            .companyType(CompanyType.MID_SIZED)
-            .recruitingStatus(RecruitingStatus.ONGOING)
-            .businessType(BusinessType.PLATFORM)
-            .score(100)
-            .employeeCount(50)
-            .annualSalary(60000)
-            .startingSalary(40000)
-            .revenue(1000000000L)
-            .operatingProfit(200000000L)
-            .ir("IR 정보")
-            .rating(4.5)
-            .benefits(null)
-            .build();
-    }
 }
