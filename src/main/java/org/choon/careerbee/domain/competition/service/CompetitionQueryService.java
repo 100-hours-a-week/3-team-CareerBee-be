de0978e6996174ca9f083b98484aa518fa6b8b8b
박성춘 <songchoon10@naver.com>
2025-05-30 16:37:16 +0900

    feat(#136): 대회 참여 여부 조회기능 구현

--- src/main/java/org/choon/careerbee/domain/competition/service/CompetitionQueryService.java
+++ src/main/java/org/choon/careerbee/domain/competition/service/CompetitionQueryService.java
@@ -1,5 +1,10 @@
 package org.choon.careerbee.domain.competition.service;
 
+import org.choon.careerbee.domain.competition.dto.response.CompetitionParticipationResp;
+
 public interface CompetitionQueryService {
 
+    CompetitionParticipationResp checkCompetitionParticipationById(
+        Long competitionId, Long accessMemberId
+    );
 }
