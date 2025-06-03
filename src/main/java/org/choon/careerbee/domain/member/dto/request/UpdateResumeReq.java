0a2df9eef48c83c1f6629da2c7fce267179f2a9b
박성춘 <songchoon10@naver.com>
2025-06-03 15:47:26 +0900

    feat(#145): 이력 정보 수정 기능 구현

---
+++ src/main/java/org/choon/careerbee/domain/member/dto/request/UpdateResumeReq.java
@@ -0,0 +1,13 @@
+package org.choon.careerbee.domain.member.dto.request;
+
+import org.choon.careerbee.domain.member.entity.enums.MajorType;
+
+public record UpdateResumeReq(
+    int certificationCount,
+    int projectCount,
+    MajorType majorType,
+    String companyName,
+    int workPeriod,
+    String position,
+    String additionalExperiences
+) {}
--- src/main/java/org/choon/careerbee/domain/member/service/MemberCommandService.java
+++ src/main/java/org/choon/careerbee/domain/member/service/MemberCommandService.java
@@ -1,9 +1,12 @@
 package org.choon.careerbee.domain.member.service;
 
 import org.choon.careerbee.domain.auth.service.oauth.OAuthInfoResponse;
+import org.choon.careerbee.domain.member.dto.request.UpdateResumeReq;
 import org.choon.careerbee.domain.member.entity.Member;
 
 public interface MemberCommandService {
 
     Member forceJoin(OAuthInfoResponse oAuthInfoResponse);
+
+    void updateResumeInfo(UpdateResumeReq updateResumeReq, Long accessMemberId);
 }
