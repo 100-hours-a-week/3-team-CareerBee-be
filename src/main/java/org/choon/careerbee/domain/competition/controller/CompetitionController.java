b296c9491b4032834487334ee23bdfeb9a0547a9
박성춘 <songchoon10@naver.com>
2025-06-05 21:49:45 +0900

    refactor: 테스트 용이성 및 기능 확장을 위해 파라미터 방식으로 날짜를 받아오도록 변경

--- src/main/java/org/choon/careerbee/domain/competition/controller/CompetitionController.java
+++ src/main/java/org/choon/careerbee/domain/competition/controller/CompetitionController.java
@@ -1,6 +1,7 @@
 package org.choon.careerbee.domain.competition.controller;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.choon.careerbee.common.dto.CommonResponse;
 import org.choon.careerbee.common.dto.CommonResponseEntity;
@@ -14,6 +15,8 @@ import org.choon.careerbee.domain.competition.dto.response.CompetitionRankingRes
 import org.choon.careerbee.domain.competition.dto.response.MemberRankingResp;
 import org.choon.careerbee.domain.competition.service.CompetitionCommandService;
 import org.choon.careerbee.domain.competition.service.CompetitionQueryService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -88,8 +92,12 @@ public class CompetitionController {
     }
 
     @GetMapping("competitions/rankings")
-    public ResponseEntity<CommonResponse<CompetitionRankingResp>> fetchCompetitionRankings() {
-        CompetitionRankingResp response = queryService.fetchRankings(LocalDate.now());
+    public ResponseEntity<CommonResponse<CompetitionRankingResp>> fetchCompetitionRankings(
+        @RequestParam("date")
+        @DateTimeFormat(iso = ISO.DATE_TIME)
+        LocalDateTime today
+    ) {
+        CompetitionRankingResp response = queryService.fetchRankings(today);
 
         return CommonResponseEntity.ok(
             response,
