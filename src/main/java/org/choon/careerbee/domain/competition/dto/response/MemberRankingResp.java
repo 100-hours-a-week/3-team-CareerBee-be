1cc732bee305eb34a625f86df6b3f965045f3252
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-06-05 16:05:40 +0900

    대회 결과 집계 테이블 컬럼 변경 및 랭킹 조회 관련 응답 필드 변경
    * refactor: 대회 결과 집계 테이블 entity에 컬럼 추가
    
    Detail
    - 최대 연속 일수
    - 참여 일수
    - 정답률
    
    * refactor: 랭킹 조회시 주간 및 월간에 해당하는 응답 필드 변
    
    * refactor: 내 랭킹 조회시 응답 필드 변

--- src/main/java/org/choon/careerbee/domain/competition/dto/response/MemberRankingResp.java
+++ src/main/java/org/choon/careerbee/domain/competition/dto/response/MemberRankingResp.java
@@ -1,9 +1,24 @@
 package org.choon.careerbee.domain.competition.dto.response;
 
 public record MemberRankingResp(
-    Long day,
-    Long week,
-    Long month
+    MemberDayRankInfo daily,
+    MemberWeekAndMonthRankInfo week,
+    MemberWeekAndMonthRankInfo month
 ) {
 
+    public record MemberDayRankInfo(
+        Long rank,
+        Long elapsedTime,
+        short solvedCount
+    ) {
+
+    }
+
+    public record MemberWeekAndMonthRankInfo(
+        Long rank,
+        Integer continuous,
+        Double correctRate
+    ) {
+
+    }
 }
