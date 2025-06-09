31480f59a291d9884e99a1c52fdf6269a9173575
박성춘 <songchoon10@naver.com>
2025-06-09 17:46:26 +0900

    feat(#144): ai 서버응답 매핑하기 위해 Json 전략 변

--- src/main/java/org/choon/careerbee/domain/member/dto/response/ExtractResumeResp.java
+++ src/main/java/org/choon/careerbee/domain/member/dto/response/ExtractResumeResp.java
@@ -1,9 +1,12 @@
 package org.choon.careerbee.domain.member.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import org.choon.careerbee.domain.member.entity.enums.MajorType;
 
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record ExtractResumeResp(
     Integer certificationCount,
     Integer projectCount,
