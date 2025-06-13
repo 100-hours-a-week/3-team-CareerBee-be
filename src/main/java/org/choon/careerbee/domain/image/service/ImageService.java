5038b1c1c4e176feb21ddd6a394997e7d8d45a76
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-06-14 01:17:38 +0900

    refactor: 회원 정보 수정시 넘겨주는 objectKey를 objectUrl 로 변환하여 정보 수정 (#205)

--- src/main/java/org/choon/careerbee/domain/image/service/ImageService.java
+++ src/main/java/org/choon/careerbee/domain/image/service/ImageService.java
@@ -2,6 +2,7 @@ package org.choon.careerbee.domain.image.service;
 
 import org.choon.careerbee.domain.image.dto.request.PresignedUrlReq;
 import org.choon.careerbee.domain.image.dto.response.GetPresignedUrlResp;
+import org.choon.careerbee.domain.image.dto.response.ObjectUrlResp;
 import org.choon.careerbee.domain.image.dto.response.PresignedUrlResp;
 import org.choon.careerbee.domain.member.dto.request.UploadCompleteReq;
 
@@ -10,4 +11,6 @@ public interface ImageService {
     PresignedUrlResp generatePresignedUrl(PresignedUrlReq presignedUrlReq);
 
     GetPresignedUrlResp generateGetPresignedUrlByObjectKey(UploadCompleteReq uploadCompleteReq);
+
+    ObjectUrlResp getObjectUrlByKey(String objectKey);
 }
