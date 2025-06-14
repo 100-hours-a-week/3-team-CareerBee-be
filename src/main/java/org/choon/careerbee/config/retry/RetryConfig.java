a6ceacab591544347d445722c7a4afb946e4bc1c
박성춘 <songchoon10@naver.com>
2025-06-15 00:07:22 +0900

    feat: 실패시 재시도를 위한 spring-retry 라이브러리 추가

---
+++ src/main/java/org/choon/careerbee/config/retry/RetryConfig.java
@@ -0,0 +1,10 @@
+package org.choon.careerbee.config.retry;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+
+}
