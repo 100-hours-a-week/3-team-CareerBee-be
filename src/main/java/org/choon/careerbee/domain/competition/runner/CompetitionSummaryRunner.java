6e70d18e56be8a29bda713177b3ef7c5722a8319
박성춘 <songchoon10@naver.com>
2025-06-18 14:46:49 +0900

    test: 15시 12분에 대회 결과 집계 시작하도록 설정

--- src/main/java/org/choon/careerbee/domain/competition/runner/CompetitionSummaryRunner.java
+++ src/main/java/org/choon/careerbee/domain/competition/runner/CompetitionSummaryRunner.java
@@ -1,5 +1,6 @@
 package org.choon.careerbee.domain.competition.runner;
 
+import io.sentry.Sentry;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,13 @@ public class CompetitionSummaryRunner {
 
     private final CompetitionSummaryService summaryService;
 
-    @Scheduled(cron = "0 0 14 * * *", zone = "Asia/Seoul")
+    //    @Scheduled(cron = "0 0 14 * * *", zone = "Asia/Seoul")
+    // 오후 3시 12분(15:12)에 실행
+    @Scheduled(cron = "0 12 15 * * *", zone = "Asia/Seoul")
     public void runCompetitionSummaryJob() {
         LocalDate now = LocalDate.now(ZoneId.of("Asia/Seoul"));
         log.info("[{}] 대회 결과 데이터 집계 시작!", now);
+        Sentry.captureMessage("대회 결과 데이터 집계 시작");
         summaryService.dailySummary(now);
 
         summaryService.weekAndMonthSummary(DateUtil.getPeriod(now, SummaryType.WEEK),
@@ -28,5 +32,6 @@ public class CompetitionSummaryRunner {
         summaryService.weekAndMonthSummary(DateUtil.getPeriod(now, SummaryType.MONTH),
             SummaryType.MONTH);
         log.info("[{}] 대회 결과 데이터 집계 종료!", now);
+        Sentry.captureMessage("대회 결과 데이터 집계 종료");
     }
 }
