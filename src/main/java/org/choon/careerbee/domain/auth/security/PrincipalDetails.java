39e497e290fefc728704f4b2a5c2a6c744ce6f95
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-06-24 11:08:01 +0900

    Refactor/58 (#239)
    * refactor: 인증객체 (UserDetails)를 Entity에서 DTO로 변경하여 쿼리 최적화 진행
    
    * test: jmeter 테스트를 위해 생성한 테스트코드
    
    * env: 레디스를 위한 환경설정
    
    * refactor: 로그인시 RT를 redis에 보관하도록 변경
    
    * refactor: redis기반으로 토큰 재발급 로직 리팩토링
    
    * refactor: redis기반으로 로그아웃 기능 리팩토링
    
    * refactor: 블랙리스트 토큰 검증시 redis 기반으로 리팩토링
    
    * delete: 토큰 저장소를 Redis로 변경하며 Token 엔티티 삭제
    
    * test: 토큰 저장소 변경에 따른 테스트코드 변경
    
    * env: 레디스 도입에 따른 환경별 변수설정

--- src/main/java/org/choon/careerbee/domain/auth/security/PrincipalDetails.java
+++ src/main/java/org/choon/careerbee/domain/auth/security/PrincipalDetails.java
@@ -3,7 +3,7 @@ package org.choon.careerbee.domain.auth.security;
 import java.util.ArrayList;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
-import org.choon.careerbee.domain.member.entity.Member;
+import org.choon.careerbee.domain.auth.dto.internal.MemberAuthInfo;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,12 +11,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 @RequiredArgsConstructor
 public class PrincipalDetails implements UserDetails {
 
-    private final Member member;
+    private final MemberAuthInfo authInfo;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Collection<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(member.getRole().toString()));
+        authorities.add(new SimpleGrantedAuthority(authInfo.role().toString()));
         return authorities;
     }
 
@@ -31,6 +31,6 @@ public class PrincipalDetails implements UserDetails {
     }
 
     public Long getId() {
-        return member.getId();
+        return authInfo.memberId();
     }
 }
