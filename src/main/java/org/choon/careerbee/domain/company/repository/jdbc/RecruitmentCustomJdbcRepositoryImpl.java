06884d9e0e15f570d27ca3c074e81631d7bb0dce
박성춘 <songchoon10@naver.com>
2025-07-04 13:23:59 +0900

    delete: 공고 batch insert시 chunk 삭제하고 최대 110개의 공고를 한번에 insert하도록 변경

--- src/main/java/org/choon/careerbee/domain/company/repository/jdbc/RecruitmentCustomJdbcRepositoryImpl.java
+++ src/main/java/org/choon/careerbee/domain/company/repository/jdbc/RecruitmentCustomJdbcRepositoryImpl.java
@@ -23,36 +23,35 @@ public class RecruitmentCustomJdbcRepositoryImpl implements
         ON DUPLICATE KEY UPDATE id = id
         """;
 
-    private static final int CHUNK = 500;
-
     private final JdbcTemplate jdbcTemplate;
+
 
     @Override
     @Transactional
     public void batchInsert(List<Recruitment> recruitments) {
-        if (recruitments.isEmpty()) return;
-
-        for (int from = 0; from < recruitments.size(); from += CHUNK) {
-            int to = Math.min(from + CHUNK, recruitments.size());
-            List<Recruitment> sub = recruitments.subList(from, to);
-
-            jdbcTemplate.batchUpdate(INSERT_SQL, new BatchPreparedStatementSetter() {
-                @Override
-                public void setValues(PreparedStatement ps, int i) throws SQLException {
-                    Recruitment recruitment = sub.get(i);
-                    ps.setLong  (1, recruitment.getCompany().getId());
-                    ps.setLong  (2, recruitment.getRecruitingId());
-                    ps.setString(3, recruitment.getUrl());
-                    ps.setString(4, recruitment.getTitle());
-
-                    LocalDateTime s = recruitment.getStartDate();
-                    LocalDateTime e = recruitment.getEndDate();
-                    ps.setTimestamp(5, java.sql.Timestamp.valueOf(s));
-                    ps.setTimestamp(6, java.sql.Timestamp.valueOf(e));
-                }
-                @Override
-                public int getBatchSize() { return sub.size(); }
-            });
+        if (recruitments.isEmpty()) {
+            return;
         }
+
+        jdbcTemplate.batchUpdate(INSERT_SQL, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                Recruitment recruitment = recruitments.get(i);
+                ps.setLong(1, recruitment.getCompany().getId());
+                ps.setLong(2, recruitment.getRecruitingId());
+                ps.setString(3, recruitment.getUrl());
+                ps.setString(4, recruitment.getTitle());
+
+                LocalDateTime s = recruitment.getStartDate();
+                LocalDateTime e = recruitment.getEndDate();
+                ps.setTimestamp(5, java.sql.Timestamp.valueOf(s));
+                ps.setTimestamp(6, java.sql.Timestamp.valueOf(e));
+            }
+
+            @Override
+            public int getBatchSize() {
+                return recruitments.size();
+            }
+        });
     }
 }
