181dc5cc889ee11619eb730475cb6ee984d0ac98
박성춘 <songchoon10@naver.com>
2025-07-07 13:46:30 +0900

    test: 회원 티켓 수량 조회에 Controller Layer 통합테스트 작성

--- src/test/java/org/choon/careerbee/domain/store/controller/StoreControllerTest.java
+++ src/test/java/org/choon/careerbee/domain/store/controller/StoreControllerTest.java
@@ -1,13 +1,18 @@
 package org.choon.careerbee.domain.store.controller;
 
+import static org.choon.careerbee.fixture.MemberFixture.createMember;
 import static org.choon.careerbee.fixture.ticket.TicketFixture.createTicket;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
+import org.choon.careerbee.domain.auth.entity.enums.TokenType;
+import org.choon.careerbee.domain.member.entity.Member;
+import org.choon.careerbee.domain.member.repository.MemberRepository;
 import org.choon.careerbee.domain.store.domain.enums.TicketType;
 import org.choon.careerbee.domain.store.repository.TicketRepository;
+import org.choon.careerbee.util.jwt.JwtUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +35,13 @@ class StoreControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
     private TicketRepository ticketRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     @DisplayName("티켓 수량 조회 API가 정상적으로 티켓 수량을 반환한다")
@@ -52,5 +63,24 @@ class StoreControllerTest {
             .andExpect(jsonPath("$.data.redCount").value(3))
             .andExpect(jsonPath("$.data.greenCount").value(5))
             .andExpect(jsonPath("$.data.blueCount").value(7));
+    }
+
+
+    @Test
+    @DisplayName("회원 티켓 수량 조회 API - 티켓 구매 이력이 없을 경우 0으로 반환")
+    void fetchMemberTicketQuantity_noPurchaseHistory_returnsZero() throws Exception {
+        // given
+        Member member = memberRepository.save(createMember("member2", "nopurchase@test.com", 2L));
+        String token = "Bearer " + jwtUtil.createToken(member.getId(), TokenType.ACCESS_TOKEN);
+
+        // when & then
+        mockMvc.perform(get("/api/v1/members/tickets")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", token))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.message").value("회원 티켓 수량 조회에 성공하였습니다."))
+            .andExpect(jsonPath("$.data.redCount").value(0))
+            .andExpect(jsonPath("$.data.greenCount").value(0))
+            .andExpect(jsonPath("$.data.blueCount").value(0));
     }
 }
