90d3b33f3243df7aec9ad8fbc347a26828be01b3
SungchoonPark <85443642+SungchoonPark@users.noreply.github.com>
2025-08-14 19:46:22 +0900

    refactor: DB CUD 작업을 위한 새 트랜잭션 적용(REQUIRED_NEW) (#280)

--- src/main/java/org/choon/careerbee/domain/notification/service/listener/PointNotifier.java
+++ src/main/java/org/choon/careerbee/domain/notification/service/listener/PointNotifier.java
@@ -4,8 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.choon.careerbee.common.pubsub.RedisPublisher;
 import org.choon.careerbee.domain.competition.dto.event.PointEvent;
-import org.choon.careerbee.domain.notification.entity.Notification;
-import org.choon.careerbee.domain.notification.repository.NotificationRepository;
+import org.choon.careerbee.domain.notification.service.NotificationCommandService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -15,22 +14,14 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class PointNotifier {
 
-    private final NotificationRepository notificationRepository;
     private final RedisPublisher redisPublisher;
+    private final NotificationCommandService commandService;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void on(PointEvent pointEvent) {
         log.info("포인트 획득 알림 전송 시작");
 
-        notificationRepository.save(
-            Notification.of(
-                pointEvent.member(),
-                String.valueOf(pointEvent.point()),
-                pointEvent.type(),
-                false
-            )
-        );
-
+        commandService.saveNoti(pointEvent);
         redisPublisher.publishPointEvent(pointEvent);
     }
 }
